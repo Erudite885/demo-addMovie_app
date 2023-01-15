a5542858a7f6f598b35a957358cb6876a904450a
Erudite885 <erudite885@gmail.com>
2023-01-15 11:54:11 +0100

    input validation logic

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -5,7 +5,13 @@ const addMovieHandler = () => {
   const title = document.getElementById('title').value;
   const extraName = document.getElementById('extra-name').value;
   const extraValue = document.getElementById('extra-value').value;
-}
+
+  if (
+    title.trim() === '' || 
+    extraName.trim() === '' || 
+    extraValue.trim() === ''){
+    return;
+  }
 
 addMovieBtn.addEventListener('click',);
 searchBtn.addEventListener('click',)
