592ce578f78df739e4a8e517c8b2539f2077eff5
Erudite885 <erudite885@gmail.com>
2023-01-15 11:51:34 +0100

    extracting the input values from the dom

---
+++ assets/scripts/objects.js
@@ -0,0 +1,11 @@
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+}
+
+addMovieBtn.addEventListener('click',);
+searchBtn.addEventListener('click',)
